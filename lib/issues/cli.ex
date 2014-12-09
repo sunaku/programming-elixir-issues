0c1a041461052fcd5d61aa58369714a284266e1a
Suraj N. Kurapati <sunaku@gmail.com>
2014-12-09 11:37:35 -0800

    OrganizingAProject-2

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -17,33 +17,39 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
   end
 
   @doc """
-  `argv` can be -h or --help, which returns :help.
+  `argv` can be -h or --help, which returns   `:help`.
 
   Otherwise it is a github user name, project name, and (optionally)
-  the number of entries to format.
+  the number of entries to format
 
-  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  Return a tuple of `{ user, project, count }`, or `nil` if help was given.
   """
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [ help: :boolean],
                                      aliases:  [ h:    :help   ])
     case  parse  do
 
-    { [ help: true ], _, _ }
-      -> :help
-
-    { _, [ user, project, count ], _ }
-      -> { user, project, String.to_integer(count) }
-
-    { _, [ user, project ], _ }
-      -> { user, project, @default_count }
-
-    _ -> :help
-
+    { [ help: true ], _,           _ } -> :help
+    { _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
+    { _, [ user, project ],        _ } -> { user, project, @default_count }
+    _                                  -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage:  issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
--- mix.exs
+++ mix.exs
@@ -1,30 +1,29 @@
+#---
+# Excerpted from "Programming Elixir",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/elixir for more book information.
+#---
 defmodule Issues.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :issues,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps]
+    [ app:     :issues,
+      version: "0.0.1",
+      elixir:  ">= 0.0.0",
+      deps:    deps ]
   end
 
   # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [ applications: [ :logger, :httpoison ] ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      { :httpoison, "~> 0.4.0" }
+    ]
   end
 end
